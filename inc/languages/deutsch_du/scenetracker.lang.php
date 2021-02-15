8e00ff5b4f51e8fc7fd19c258435801c81fcaa1e
Katja <47885564+katjalennartz@users.noreply.github.com>
2021-02-15 13:23:42 +0100

    Add files via upload

---
+++ inc/languages/deutsch_du/scenetracker.lang.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Social Network 1.0 Main Language File
+ */
+
+$l['myalerts_setting_scenetracker_newScene'] = 'Benachrichtung bei einer neuen Szene?';
+$l['scenetracker_newScene'] = '{1} hat eine Szene eröffnet.';
+
+$l['myalerts_setting_scenetracker_newAnswer'] = 'Benachrichtung bei einer neuen Antwort?';
+$l['scenetracker_newAnswer'] = '{1} hat auf eine Szene geantwortet.';
+
+
+
+?>
