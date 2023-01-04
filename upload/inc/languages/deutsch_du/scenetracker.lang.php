223c8105517b73c3f171a248fe3b920fadaa47af
Katja <47885564+katjalennartz@users.noreply.github.com>
2023-01-04 08:51:12 +0100

    adding missing language variables

--- upload/inc/languages/deutsch_du/scenetracker.lang.php
+++ upload/inc/languages/deutsch_du/scenetracker.lang.php
@@ -3,12 +3,45 @@
  * Social Network 1.0 Main Language File
  */
 
+/*base*/
+$l['scenetracker_btnsubmit'] = 'speichern';
+
+/*showthread*/ 
+$l['scenetracker_openscene'] = '[open]';
+$l['scenetracker_openscenestatus'] = 'scene is open.';
+$l['scenetracker_closescene'] = '[close]';
+$l['scenetracker_closescenestatus'] = 'scene is closed.';
+$l['scenetracker_editinfos'] = '[edit infos]';
+$l['scenetracker_delete'] = '[x]';
+$l['scenetracker_triggeredit'] = 'Trigger:';
+$l['scenetracker_teilnehmer'] = 'Teilnehmer hinzufügen';
+$l['scenetracker_trigger'] = 'Triggerwarnung';
+$l['scenetracker_place'] = 'Ort';
+
+/*UCP*/
+$l['scenetracker_closescene'] = '[close]';
+$l['scenetracker_reminderopt'] = 'Szenenerinnerung nach {1} Tage(n)?';
+$l['scenetracker_select_allChars'] = 'allen Charas';
+
+$l['scenetracker_select_another'] = 'another user posted';
+$l['scenetracker_select_never'] = 'never';
+
+$l['scenetracker_showstring_all'] = 'Alle Charaktere';
+$l['scenetracker_showstring_characount'] = '(#{1})';
+$l['scenetracker_showstring'] = 'Deine Szenen ({1} - {2} - dran? {3} - #{4})';
+$l['scenetracker_yourturn'] = '(Du bist dran)';
+$l['scenetracker_alert'] = '[alert]';
+$l['scenetracker_alerttypealways'] = 'immer';
+$l['scenetracker_alerttypenever'] = 'nie';
+
+
+$l['scenetracker_displaystatus_shown'] = 'is displayed (profile)';
+$l['scenetracker_displaystatus_hidden'] = 'is hidden';
+$l['scenetracker_sceneisclosed'] = 'is closed';
+$l['scenetracker_sceneisopen'] = 'is open';
+
+/*my alerts*/
 $l['myalerts_setting_scenetracker_newScene'] = 'Szenentracker: Benachrichtigung bei einer neuen Szene?';
 $l['scenetracker_newScene'] = '{1} hat eine Szene eröffnet oder du wurdest nachträglich hinzugefügt.';
-
 $l['myalerts_setting_scenetracker_newAnswer'] = 'Szenentracker: Benachrichtigung bei einer neuen Antwort?';
 $l['scenetracker_newAnswer'] = '{1} hat auf eine Szene geantwortet.';
-
-
-
-?>
