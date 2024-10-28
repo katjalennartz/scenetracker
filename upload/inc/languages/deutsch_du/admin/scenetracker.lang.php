d55ebb29a46cd3ba1defc62dcb9430e700e08c5f
Katja <47885564+katjalennartz@users.noreply.github.com>
2024-10-28 12:38:08 +0100

    Create scenetracker.lang.php

---
+++ upload/inc/languages/deutsch_du/admin/scenetracker.lang.php
@@ -0,0 +1,16 @@
+<?php
+$l['scenetracker_import'] = 'Szenentracker Import';
+$l['scenetracker_permission'] = 'Szenentracker: Darf Szenen importieren und Updates durchführen.';
+
+$l['scenetracker_modul_dscr'] ="Hier kannst du Szenen aus anderen Plugins übertragen. Mach <b>unbedingt</b> vorher ein Datenbankbackup und sichere die FTP Dateien.";
+
+$l['scenetracker_tracker_select'] = 'Trackersystem ';
+$l['scenetracker_tracker_select_descr'] = "Wähle aus von welchem Trackersystem du Szenen importieren möchtest. <b>Bitte mach vorher ein vollständiges Backup!</b>";
+$l['scenetracker_error_select_tracker'] = "Wähle einen Tracker aus.";
+$l['scenetracker_error_ipt3'] = "Du hast den IPT3 von Jule ausgewählt. Die Tabelle des Trackers ist aber nicht vorhanden.";
+$l['scenetracker_error_ipt2'] = "Du hast den IPT2 von Jule ausgewählt. Die Datenbankfelder sind aber nicht vorhanden.";
+
+$l['scenetracker_success'] = "Szenen wurden übertragen.";
+
+$l['scenetracker_btnsubmit'] = 'Szenen übertragen';
+
