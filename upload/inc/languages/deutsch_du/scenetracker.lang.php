8b115ab5188d54a524c66a46efed6bc471a09d7a
Katja <47885564+katjalennartz@users.noreply.github.com>
2025-04-03 14:29:52 +0200

    Add files via upload

--- upload/inc/languages/deutsch_du/scenetracker.lang.php
+++ upload/inc/languages/deutsch_du/scenetracker.lang.php
@@ -20,7 +20,8 @@ $l['scenetracker_showstring_characount'] = '(#{1})';
 $l['scenetracker_showstring'] = 'Deine Szenen ({1} - {2} - dran? {3} - mit {4} - #{5})';
 $l['scenetracker_yourturn'] = '(Du bist dran)';
 $l['scenetracker_alert'] = '[alert]';
-$l['scenetracker_alerttypealways'] = 'immer';
+$l['scenetracker_alerttypealways'] = 'anderer user';
+$l['scenetracker_alerttypealways_always'] = 'immer';
 $l['scenetracker_ucp_alerttype_index'] = '<b>Index:</b> ';
 $l['scenetracker_ucp_alerttype_alert'] = '<b>Alert:</b> ';
 $l['scenetracker_alerttypenever'] = 'nie';
@@ -30,6 +31,7 @@ $l['scenetracker_sceneisclosed'] = 'is closed';
 $l['scenetracker_sceneisopen'] = 'is open';
 $l['scenetracker_index_view_reminder_on'] = 'indexreminder';
 $l['scenetracker_index_view_reminder_off'] = 'no indexreminder';
+$l['scenetracker_alersetting_always_always'] = 'always';
 $l['scenetracker_alersetting_always'] = 'another user posted';
 $l['scenetracker_alersetting_never'] = 'never';
 
